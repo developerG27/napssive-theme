0ee9ffff1565f62fb719d6d60f4b8caacfa2d0c5
Gian Pumayalla <g.pumayalla@hotmail.com>
2019-01-29 17:18:12 +0100

    articolo

--- index.php
+++ index.php
@@ -6,7 +6,13 @@
 
 <section class="main-container">
 	<?php get_sidebar();?>
-	fdsfsdf
+	<div class="row lg">
+		<div class="container">
+			<div class="card card-lg">
+				<h3>Articolo</h3>			
+			</div>
+		</div>
+	</div>
 </section>
 </body>
 </html>
