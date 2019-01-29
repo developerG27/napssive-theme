0bc1c6d3d89a41466ff7809f1b6e6a77fd03961c
Gian Pumayalla <g.pumayalla@hotmail.com>
2019-01-29 17:11:06 +0100

    creata sidebar

--- index.php
+++ index.php
@@ -5,20 +5,8 @@
 
 
 <section class="main-container">
-	<aside class="aside">
-		<?php query_posts('compass'); ?>
-			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-			<ul>
-				<li> 
-					<a href="<?php the_permalink(); ?>">
-						<article <?php post_class('lista-titolo'); ?>>
-							<p class="title"><?php the_title(); ?></p>
-						</article>
-					</a>
-				</li>
-			</ul>
-      <?php endwhile; endif; ?>
-	</aside>
+	<?php get_sidebar();?>
+	fdsfsdf
 </section>
 </body>
 </html>
