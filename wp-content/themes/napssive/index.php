ad05184f45eed1b5baa8016f01938a9e1679a30e
Gian Pumayalla <g.pumayalla@hotmail.com>
2019-02-11 17:03:46 +0100

    inserita sidebar in index

--- wp-content/themes/napssive/index.php
+++ wp-content/themes/napssive/index.php
@@ -5,7 +5,16 @@
 
 
 <section class="main-container">
-	<?php get_sidebar();?>
+	<aside class="aside">
+  <?php 
+    $categories = get_categories();
+    
+    foreach($categories as $category) {
+      echo '<ul> <li> <a href="' . get_category_link($category->term_id) . '">' . $category->name . '</a> </li> </ul>'; 
+    } 
+  ?>  
+
+</aside>
 
 	<div class="content">
 		<div class="row">
