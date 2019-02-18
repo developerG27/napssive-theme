5b3aeec67ec1022aaa4c5078423713cae4722e8b
Gian Pumayalla <g.pumayalla@hotmail.com>
2019-02-18 14:17:20 +0100

    Tabulazione corretta - aggiunto readme dove trovare i shortcut

--- functions.php
+++ functions.php
@@ -1,51 +1,59 @@
 <?php
 //Al posto di rand() aggiungerò la versione
-wp_enqueue_style( 'style', get_stylesheet_uri(), rand() );
+wp_enqueue_style('style', get_stylesheet_uri(), rand());
 
 //Carico lo script
 //il nome, src, array(), la versione, true= nel footer, false= nel header
-wp_enqueue_script('script',get_template_directory_uri().'/script/script.js',array(),rand(),false);
+wp_enqueue_script('script', get_template_directory_uri() . '/script/script.js', array(), rand(), false);
 
 //Aggiunge le post thumbnails (Immagini)
-add_theme_support('post-thumbnails');   
+add_theme_support('post-thumbnails');
 
 //Menu
-register_nav_menus( array(
+register_nav_menus(array(
   'primary' => esc_html__('primary'),
 ));
 
 // Cambio del nome del footer nel backend
-function remove_footer_admin () {
-	echo 'Sviluppato da <a href="http://www.wordpress.org" target="_blank">DeveloperG27</a>';
+function remove_footer_admin()
+{
+  echo 'Sviluppato da <a href="http://www.wordpress.org" target="_blank">DeveloperG27</a>';
 }
+
 add_filter('admin_footer_text', 'remove_footer_admin');
 
 //Messaggio quando un utente inserisce username e password sbagliati
-function no_wordpress_errors(){
+function no_wordpress_errors()
+{
   return 'Hai sbagliato!!';
 }
-add_filter( 'login_errors', 'no_wordpress_errors' );
+
+add_filter('login_errors', 'no_wordpress_errors');
 
 //rimuove la barra superiore wp_head()
 add_action('get_header', 'my_filter_head');
-function my_filter_head() {
-	remove_action('wp_head', '_admin_bar_bump_cb');
-} 
+function my_filter_head()
+{
+  remove_action('wp_head', '_admin_bar_bump_cb');
+}
 
 //Abitalita i blocchi avanzati di gutemberg
-function my_custom_languages( $languages ) {
-  $languages[] = array('slug' => 'rust', 'mode' => 'rust', 'label' => 'Rust') ;
+function my_custom_languages($languages)
+{
+  $languages[] = array('slug' => 'rust', 'mode' => 'rust', 'label' => 'Rust');
   return $languages;
 }
-add_filter( 'advanced_gutenberg_blocks_code_languages', 'my_custom_languages' );
+
+add_filter('advanced_gutenberg_blocks_code_languages', 'my_custom_languages');
 
 //costringe ad usare il certificato ssl
-define('FORCE_SSL_ADMIN', true); 
+define('FORCE_SSL_ADMIN', true);
 
 // SOLO IN SVILUPPO, ELIMINARE IN PRODUZIONE-------------------------------------------------------------------
 // Elimina la versione di wp dal url: maggior sicurezza
-function remove_src_version ( $url ) {
-  $url = preg_replace('/([?&])' . 'ver' . '=[^&]+(&|$)/','$1',$url);
+function remove_src_version($url)
+{
+  $url = preg_replace('/([?&])' . 'ver' . '=[^&]+(&|$)/', '$1', $url);
   if (preg_match("/\?$/", $url) || preg_match("/\&$/", $url))
     return substr($url, 0, -1);
   else
---
+++ readme.md
@@ -0,0 +1,6 @@
+Commit                      Ctrl + K
+Push                        Ctrl + Shift + K     
+Pull                        Ctrl + T
+Codice ordinato             Ctrl + Alt + L
+Cerca qualsiasi file        Shift + Shif
+File recenti                Ctrl + E
