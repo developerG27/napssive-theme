9cf9d99007b7e16bf6bd5eeab357cfdf7cbdd943
Gian Pumayalla <g.pumayalla@hotmail.com>
2019-02-18 09:16:56 +0100

    separati file di wp

--- 404.php
+++ 404.php
@@ -4,7 +4,7 @@
 ?>
 
 <section class="main-container">
-	<aside class="aside">
+  <aside class="aside">
     <?php 
       $categories = get_categories();
       
@@ -17,19 +17,8 @@
 	<div class="content">
 		<div class="row">
 			<div class="card card-md">
-
-        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-
-        <a href="<?php the_permalink(); ?>">
-          <p><?php the_title(); ?></p>
-        </a>
-
-        <?php endwhile; else: ?>
-
-          0 risultati
-
-        <?php endif; ?>
-			</div>
+        <p>Questa pagina non esiste </p>
+			</div>  
 		</div>
 	</div>
 </section>
--- wp-content/themes/web-wp/footer.php
+++
@@ -1,5 +0,0 @@
-<footer class="end">
-      <div class="container">
-         <a href="<?php echo home_url(); ?>"><p class="copyright"> Blog &#169; 2019 </p></a>
-      </div>
-</footer>
