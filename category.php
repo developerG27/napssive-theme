21ab93acef9c183a48631e49b8799fa81582cf52
Gian Pumayalla <g.pumayalla@hotmail.com>
2019-02-19 21:55:27 +0100

    fix menu header

--- category.php
+++ category.php
@@ -5,13 +5,13 @@ get_header();
 <section class="main-container">
   <?php get_sidebar(); ?>
 
-  <div class="content">
-    <div class="row">
-      <div class="card card-md">
-        <p> <- Scegli l'articolo che preferisci</p>
-      </div>
+    <div class="content">
+        <div class="row">
+            <div class="card card-md">
+                <p> <- Scegli l'articolo che preferisci</p>
+            </div>
+        </div>
     </div>
-  </div>
 </section>
 
 </body>
