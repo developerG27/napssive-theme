54e1319421d8549ecb9f25ca417892e38e6e643b
Gian Pumayalla <g.pumayalla@hotmail.com>
2019-02-20 10:02:45 +0100

    separato respository javascript da napssive

--- javascript/esercizi/semplici/tabella-moltiplicazione.js
+++
@@ -1,7 +0,0 @@
-//In base ad un numero inserito dall'utente far apparire la sua tabella x 10
-
-var numeroUtente = parseInt(prompt('Inserisci un numero'))
-
-for(var i = 1; i<= 10; i++){
-    console.log(`${numeroUtente} x ${i} = ${numeroUtente*i}`)
-}
