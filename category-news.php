acef1c35cd66f60b4087bb7198ab64cfc86e208b
Gian Pumayalla <g.pumayalla@hotmail.com>
2019-02-25 14:19:18 +0100

    Articoli allineati

--- category-news.php
+++ category-news.php
@@ -12,17 +12,20 @@ get_header();
       <div class="card card-md news">
         <?php get_search_form(); ?>
 
+        <div class="box-article">
         <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
+
           <a href="<?php the_permalink(); ?>">
-            <?php the_post_thumbnail(); ?>
-            <p><?php the_title(); ?></p>
+            <article class="article">
+              <?php the_post_thumbnail(); ?>
+              <p class="title"> <?php the_title(); ?></p>
+            </article>
           </a>
 
         <?php endwhile; else: ?>
-
+        </div>
           0 risultati
-
         <?php endif; ?>
       </div>
     </div>
