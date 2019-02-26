2a4bbcacedec256b81528a49ed8721efd7bb2d2a
Gian Pumayalla <g.pumayalla@hotmail.com>
2019-02-26 15:02:22 +0100

    sistemate immagini in single

--- footer.php
+++ footer.php
@@ -1,5 +1,7 @@
 <footer class="end">
   <div class="container">
-    <a href="<?php echo home_url(); ?>"><p class="copyright"> Blog &#169; 2019 </p></a>
+    <a href="<?php echo home_url(); ?>">
+      <p class="copyright"> Blog &#169; 2019 </p>
+    </a>
   </div>
 </footer>
--- search.php
+++ search.php
@@ -9,7 +9,6 @@ get_header();
     //Rimuove categoria 14 dal loop: news
     $categories = get_categories('hide_empty=0&exclude=14');
     foreach ($categories as $category) {
-
       echo '<ul> <li> <a href="' . get_category_link($category->term_id) . '">' . $category->name . '</a> </li> </ul>';
     }
     ?>
@@ -19,16 +18,11 @@ get_header();
     <div class="row">
       <div class="card card-md">
         <?php get_search_form(); ?>
-        <p>Hai cercato <?php echo $ricerca ?></p>
-
+          <p>Hai cercato <?php echo $ricerca ?></p>
         <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-
-            <p><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></p>
-
+          <p><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></p>
         <?php endwhile; else: ?>
-
           0 risultati
-
         <?php endif; ?>
       </div>
     </div>
