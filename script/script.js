796285ddaf3c6cc791892c88911995d4b01c0ca3
Gian Pumayalla <g.pumayalla@hotmail.com>
2019-05-05 01:45:57 +0200

    componente aside

--- script/script.js
+++ script/script.js
@@ -1,14 +1,3 @@
-// var $ = jQuery;
-// $(document).ready(function(){
-//     console.log('ready')
-
-//     $('#app-menu').on('click',function(){
-//         console.log('hai fatto click')
-
-//         $('#secondary-menu').toggleClass('none');
-//     })
-// })
-
 let burgherMenu = document.querySelector('#app-menu')
 let menu = document.querySelector("#menu");
 burgherMenu.addEventListener('click', () => {
